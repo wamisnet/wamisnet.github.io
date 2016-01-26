918f995c06cba2847956596574cd53fa3254e834
wami <wamiwami@live.jp>
2016-01-26 23:21:54 +0900

    no message

---
+++ arduino/sketch_jan23a.cpp
@@ -0,0 +1,15 @@
+#line 1 "sketch_jan23a.ino"
+#include "Arduino.h"
+void setup();
+void loop();
+#line 1
+void setup() {
+                                           
+
+}
+
+void loop() {
+                                                
+
+}
+
