eff5d234b2e5f73cc97b2c54d47ff73a0b9ff6e3
wami <wamiwami@live.jp>
2016-08-28 08:55:15 +0900

    no message

--- pg/memory_clear/memory_clear.ino
+++ pg/memory_clear/memory_clear.ino
@@ -7,6 +7,8 @@ void setup() {
     Nefry.setConfValue(0, i);
     Nefry.setConfStr("", i + 1);
   }
+  Nefry.setLed(0,255,0);
+  if(WiFi.status() != WL_CONNECTED)Nefry.reset();
   Nefry.setWebUpdate("wamisnet.github.io","/pg/default");
 }
 
