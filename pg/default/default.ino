7d9e382350fa499ecadb145c044e36aaa46432fd
wami <wamiwami@live.jp>
2016-08-28 08:26:30 +0900

    no message

---
+++ pg/default/default.ino
@@ -0,0 +1,11 @@
+#include <Nefry.h>
+
+void setup() {
+  // put your setup code here, to run once:
+Nefry.setProgramName("Default Program");
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  Nefry.setLed(random(250),random(250),random(250));
+}
---
+++ pg/memory_clear/memory_clear.ino
@@ -0,0 +1,15 @@
+#include <Nefry.h>
+void setup() {
+  Nefry.setConfWifi("Nefry","Nefry-wifi");
+  Nefry.setConfModule("","","");
+  Nefry.setConfUser("","");
+  for (int i = 0; i < 8; i++) {
+    Nefry.setConfValue(0, i);
+    Nefry.setConfStr("", i + 1);
+  }
+  Nefry.setWebUpdate("wamisnet.github.io","/pg/default");
+}
+
+void loop() {
+
+}
