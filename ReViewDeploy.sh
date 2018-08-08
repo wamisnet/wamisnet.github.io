2fe5953552bcef0e97898994bdcf1aed0bcbc203
wami <wamiwami@live.jp>
2018-08-08 20:39:03 +0900

    動作確認

--- ReViewDeploy.sh
+++ ReViewDeploy.sh
@@ -2,13 +2,12 @@
 
 go get -v github.com/spf13/hugo
 hugo
-
 git clone https://github.com/wamisnet/wamisnet.github.io.git
-
+ls
 cp public/ wamisnet.github.io/
 
 cd wamisnet.github.io
-
+ls
 git remote set-url origin git@github.com:wamisnet/wamisnet.github.io.git
 
 git config --global user.email "wamiwami@live.jp"
